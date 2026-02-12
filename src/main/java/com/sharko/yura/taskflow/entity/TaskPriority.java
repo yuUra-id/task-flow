5d40291c9696acf0122e9b962352c504555abaab
Yura Sharko <yura.sharko.04@mail.ru>
2026-02-12 17:33:24 +0500

    Created two enum TaskStatus and TaskPriority

---
+++ src/main/java/com/sharko/yura/taskflow/entity/TaskPriority.java
@@ -0,0 +1,9 @@
+package com.sharko.yura.taskflow.entity;
+
+public enum TaskPriority {
+
+    LOW,
+    MEDIUM,
+    HIGH,
+
+}
---
+++ src/main/java/com/sharko/yura/taskflow/entity/TaskStatus.java
@@ -0,0 +1,9 @@
+package com.sharko.yura.taskflow.entity;
+
+public enum TaskStatus {
+
+    NEW,
+    IN_PROGRESS,
+    COMPLETED
+
+}
