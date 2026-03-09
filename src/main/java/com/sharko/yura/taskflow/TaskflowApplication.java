1710957064953241441bb4189b4823b339c746a9
Yura Sharko <yura.sharko.04@mail.ru>
2026-03-09 13:23:46 +0500

    feat(log): add logger in project

--- src/main/java/com/sharko/yura/taskflow/TaskflowApplication.java
+++ src/main/java/com/sharko/yura/taskflow/TaskflowApplication.java
@@ -1,13 +1,28 @@
 package com.sharko.yura.taskflow;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class TaskflowApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(TaskflowApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(TaskflowApplication.class, args);
+
+        SpringApplication.run(TaskflowApplication.class, args);
+
 	}
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onStart(){
+
+        log.info("TaskFlow application started");
+
+    }
 
 }
